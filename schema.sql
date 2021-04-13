7557c2f2a7e6da4e962a6f5bdef4f50c4b867b65
Juhi Aggarwal <juhiaggarwal@Juhis-MacBook-Pro.local>
2021-04-12 22:56:13 -0500

    sql for 5 tables

--- schema.sql
+++ schema.sql
@@ -33,15 +33,6 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 )
 	
-CREATE TABLE dept_emp (
-  emp_no INT NOT NULL,
-  dept_no varchar (4) NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-)
-	
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title varchar NOT NULL,
